56f20dc7409244ab810c19eac4ae8cb39c4943c6
王成成 <wangchengcheng@hoge.cn>
2018-03-21 14:28:22 +0800

    update

--- qrcode/code.php
+++ qrcode/code.php
@@ -6,44 +6,37 @@
  * Time: 下午2:29
  */
 
-echo 333;die;
-
 include './phpqrcode.php';
 $value = 'http://www.cnblogs.com/txw1958/'; //二维码内容
 $errorCorrectionLevel = 'L';//容错级别  二维码纠错级别：L、M、Q、H
-$matrixPointSize = 6;//生成图片大小  // 二维码点的大小：1到10，用于手机端4就可以了
+$matrixPointSize = 10;//生成图片大小  // 二维码点的大小：1到10，用于手机端4就可以了
 //生成二维码图片
 ob_start();
 QRcode::png($value, FALSE, $errorCorrectionLevel, $matrixPointSize, 2);
 $logo = '2.png';//准备好的logo图片
 $cc = ob_get_contents();
 ob_end_clean();
-echo $cc;
+
 //$QR = 'qrcode.png';//已经生成的原始二维码图
 
 
 
 
-
-
-
-
-//
-//if ($logo !== FALSE) {
-//    $QR = imagecreatefromstring(file_get_contents($QR));
-//    $logo = imagecreatefromstring(file_get_contents($logo));
-//    $QR_width = imagesx($QR);//二维码图片宽度
-//    $QR_height = imagesy($QR);//二维码图片高度
-//    $logo_width = imagesx($logo);//logo图片宽度
-//    $logo_height = imagesy($logo);//logo图片高度
-//    $logo_qr_width = $QR_width / 5;
-//    $scale = $logo_width/$logo_qr_width;
-//    $logo_qr_height = $logo_height/$scale;
-//    $from_width = ($QR_width - $logo_qr_width) / 2;
-//    //重新组合图片并调整大小
-//    imagecopyresampled($QR, $logo, $from_width, $from_width, 0, 0, $logo_qr_width,
-//        $logo_qr_height, $logo_width, $logo_height);
-//}
-////输出图片
-//imagepng($QR, 'helloweixin.png');
-//echo '<img src="helloweixin.png">';
+if ($logo !== FALSE) {
+    $QR = imagecreatefromstring(file_get_contents($QR));
+    $logo = imagecreatefromstring(file_get_contents($logo));
+    $QR_width = imagesx($QR);//二维码图片宽度
+    $QR_height = imagesy($QR);//二维码图片高度
+    $logo_width = imagesx($logo);//logo图片宽度
+    $logo_height = imagesy($logo);//logo图片高度
+    $logo_qr_width = $QR_width / 5;
+    $scale = $logo_width/$logo_qr_width;
+    $logo_qr_height = $logo_height/$scale;
+    $from_width = ($QR_width - $logo_qr_width) / 2;
+    //重新组合图片并调整大小
+    imagecopyresampled($QR, $logo, $from_width, $from_width, 0, 0, $logo_qr_width,
+        $logo_qr_height, $logo_width, $logo_height);
+}
+//输出图片
+imagepng($QR, 'helloweixin.png');
+echo '<img src="helloweixin.png">';
