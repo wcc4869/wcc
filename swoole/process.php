8b1ac4f80b4309a5a2f10de0ca7e3af040c2ec54
王成成 <wangchengcheng@hoge.cn>
2018-05-22 09:00:57 +0800

    update

--- swoole/process.php
+++ swoole/process.php
@@ -6,20 +6,26 @@
  * Time: 下午12:01
  */
 
-//$workers = [];
-//
-//$swoole_worker_num = 3;
-//
-////for($i=0;$i<$swoole_worker_num;$i++){
-////    echo $i;
-//    $process = new swoole_process('doProcess',false,true);
-//    $pid = $process->start();
-//    //$workers[$pid] = $process;
-////}
-//
-// 创建进程
-function doProcess(swoole_process $worker){
-    echo "PID",$worker->pid,"\n";
+$workers = []; //进程数组
+
+$worker_num = 3; //  创建进程的数据量
+// 创建启动进程
+for ($i =0; $i < $worker_num ; $i++){
+    $process = new swoole_process('doProcess');// 创建单独新进程
+    $pid = $process ->start();     // 启动进程，并获取进程ID
+    $workers[$pid] = $process;     // 存入进程数组
 }
 
-
+// 创建进程执行函数
+function doProcess(swoole_process $process){
+    $process->write("PID: $process->pid ");  // 子进程写入信息  pipe
+    echo "写入信息： $process->pid $process->callback";
+}
+// 添加进程事件  向每一个子进程添加需要执行的动作
+foreach ($workers as $process){
+    // 添加
+    swoole_event_add($process->pipe,function($pipe)use($process){
+        $data = $process->read(); // 能否读取数据
+        echo "接收到：$data \n";
+    });
+}
